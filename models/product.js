69f9b8cab8412b75ed0ab53e7e6817b437f52c85
Gerardo Joves <gerardooenrrique@gmail.com>
2024-01-24 18:23:28 -0400

    create product model

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  categories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  number_in_stock: Number,
+});
+
+productSchema.virtual('url').get(function () {
+  return `/catalog/product/${this._id}`;
+});
+
+module.exports = mongoose.model('Product', productSchema);

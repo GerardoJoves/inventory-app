5f0f33dc547d6ca7263f82b97297b7182c9fb1cf
Gerardo Joves <gerardooenrrique@gmail.com>
2024-01-25 18:08:21 -0400

    make all controller functions async

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,33 +1,33 @@
 const asyncHandler = require('express-async-handler');
 
-exports.category_list = asyncHandler((req, res) => {
+exports.category_list = asyncHandler(async (req, res) => {
   res.send('Not implemented: category list');
 });
 
-exports.category_details = asyncHandler((req, res) => {
+exports.category_details = asyncHandler(async (req, res) => {
   res.send('Not implemented: category details');
 });
 
-exports.category_create_get = asyncHandler((req, res) => {
+exports.category_create_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: category create get');
 });
 
-exports.category_create_post = asyncHandler((req, res) => {
+exports.category_create_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: category create post');
 });
 
-exports.category_delete_get = asyncHandler((req, res) => {
+exports.category_delete_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: category delete get');
 });
 
-exports.category_delete_post = asyncHandler((req, res) => {
+exports.category_delete_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: category delete post');
 });
 
-exports.category_update_get = asyncHandler((req, res) => {
+exports.category_update_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: category update get');
 });
 
-exports.category_update_post = asyncHandler((req, res) => {
+exports.category_update_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: category update post');
 });
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,37 +1,37 @@
 const asyncHandler = require('express-async-handler');
 
-exports.index = asyncHandler((req, res) => {
+exports.index = asyncHandler(async (req, res) => {
   res.send('Not implemented: home page');
 });
 
-exports.product_list = asyncHandler((req, res) => {
+exports.product_list = asyncHandler(async (req, res) => {
   res.send('Not implemented: product list');
 });
 
-exports.product_details = asyncHandler((req, res) => {
+exports.product_details = asyncHandler(async (req, res) => {
   res.send('Not implemented: product details');
 });
 
-exports.product_create_get = asyncHandler((req, res) => {
+exports.product_create_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: product create get');
 });
 
-exports.product_create_post = asyncHandler((req, res) => {
+exports.product_create_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: product create post');
 });
 
-exports.product_delete_get = asyncHandler((req, res) => {
+exports.product_delete_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: product delete get');
 });
 
-exports.product_delete_post = asyncHandler((req, res) => {
+exports.product_delete_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: product delete post');
 });
 
-exports.product_update_get = asyncHandler((req, res) => {
+exports.product_update_get = asyncHandler(async (req, res) => {
   res.send('Not implemented: product update get');
 });
 
-exports.product_update_post = asyncHandler((req, res) => {
+exports.product_update_post = asyncHandler(async (req, res) => {
   res.send('Not implemented: product update post');
 });
